61aa5cb162b177b1773e31c33a492c41772724b9
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2021-06-09 19:56:55 +0700

    ll

--- WebApi/routes/web.php
+++ WebApi/routes/web.php
@@ -52,5 +52,5 @@ Route::get('/pengguna', 'PenggunaController@index')->name('pengguna');
 Route::get('/tambahpengguna', 'PenggunaController@create')->name('tambahpengguna');
 Route::post('/simpanpengguna', 'PenggunaController@store')->name('simpanpengguna');
 Route::get('/editpengguna/{id}', 'PenggunaController@edit')->name('editpengguna');
-Route::get('/hapuspengguna/{id}', 'PenggunaController@destroy')->name('hapuspengguna');
 Route::post('/updatepengguna/{id}', 'PenggunaController@update')->name('updatepengguna');
+Route::get('/hapuspengguna/{id}', 'PenggunaController@destroy')->name('hapuspengguna');
