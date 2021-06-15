3c62f3a52831850964f8ea93bd266fa5c9beefcd
nailahputeri <72058421+nailahputeri@users.noreply.github.com>
2021-06-14 18:59:55 -0700

    Update landingpage.blade.php

--- WebApi/resources/views/frontend/layouts/landingpage.blade.php
+++ WebApi/resources/views/frontend/layouts/landingpage.blade.php
@@ -50,7 +50,7 @@
   <div class="row">
     <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
       <br><br>
-      <h1>Creating websites that make you stop & stare</h1>
+      <h1>SI PEKA</h1>
       <h2>Accusantium quam, aliquam ultricies eget tempor id, aliquam eget nibh et. Maecen aliquam, risus at semper. Proin iaculis purus consequat sem cure digni ssim. Donec porttitora entum.</h2>
       <a href="/get app" class="btn btn-success">Get App</a>
     </div>
