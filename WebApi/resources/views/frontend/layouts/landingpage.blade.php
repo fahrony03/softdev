24d1f2f98673ebd3d2aba7c04e99f11b32e4b372
nailahputeri <72058421+nailahputeri@users.noreply.github.com>
2021-06-14 18:24:29 -0700

    LP

--- WebApi/resources/views/frontend/layouts/landingpage.blade.php
+++ WebApi/resources/views/frontend/layouts/landingpage.blade.php
@@ -10,6 +10,60 @@
 
   </head>
   <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+  
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <a href="/login" class="btn btn-success">login</a>
+      </form>
+    </div>
+  </div>
+</nav>
+  <section id="hero" class="d-flex align-items-center">
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+      <br><br>
+      <h1>Creating websites that make you stop & stare</h1>
+      <h2>Accusantium quam, aliquam ultricies eget tempor id, aliquam eget nibh et. Maecen aliquam, risus at semper. Proin iaculis purus consequat sem cure digni ssim. Donec porttitora entum.</h2>
+      <a href="/get app" class="btn btn-success">Get App</a>
+    </div>
+    <div class="col-lg-6 order-1 order-lg-2 hero-img">
+      <br><br>
+      <img src="assets/img/hero-img.png" class="img-fluid" alt="">
+    </div>
+  </div>
+</div>
+
+
+
+</section><!-- End Hero -->
 
     <!-- Optional JavaScript; choose one of the two! -->
 
@@ -22,6 +76,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
     -->
 
-    <a href="/login" class="btn btn-success">login</a>
+
   </body>
 </html>
