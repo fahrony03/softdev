e9c7c13f78fdbb04ad570f1f4ea30dd8491d2f67
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2021-07-01 09:59:54 +0700

    up

--- WebApi/routes/api.php
+++ WebApi/routes/api.php
@@ -21,18 +21,22 @@ use App\Http\Controllers\AuthController;
 // });
 
 Route::post('login', 'AuthController@login');
+
 Route::group(['prefix' => 'auth', 'middleware' => 'auth:sanctum'], function() {
     // manggil controller sesuai bawaan laravel 8
     Route::post('logout', [AuthController::class, 'logout']);
     // manggil controller dengan mengubah namespace di RouteServiceProvider.php biar bisa kayak versi2 sebelumnya
-    Route::post('logoutall', 'AuthController@logoutall');
+    Route::get('api_user', 'ApiUserController@getall');
+
+    // Route::get('api_user', [ApiUserController::class, 'getall']);
+    
 });
 
 Route::group(['namespace' => 'Backend'], function(){
-    Route::get('api_user', 'ApiUserController@getall');
-    Route::get('api_user/{id}', 'ApiUserController@getus');
-    Route::post('api_user', 'ApiUserController@createus');
-    Route::put('api_user', 'ApiUserController@updateus');
-    Route::delete('api_user', 'ApiUserController@deleteus');
-
+    // Route::get('api_user', 'ApiUserController@getall');
+    // // Route::get('api_user/{id}', 'ApiUserController@getus');
+    // Route::post('api_user', 'ApiUserController@createus');
+    // Route::put('api_user', 'ApiUserController@updateus');
+    // Route::delete('api_user', 'ApiUserController@deleteus');
+    
 });
