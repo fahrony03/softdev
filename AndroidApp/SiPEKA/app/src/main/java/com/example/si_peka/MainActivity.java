723342dd00ef6aa16ab45730c5028e7c9344d583
Moch. Fahrony <72066155+fahrony03@users.noreply.github.com>
2021-07-08 00:21:35 +0700

    Android Fix

--- AndroidApp/SiPEKA/app/src/main/java/com/example/si_peka/MainActivity.java
+++ AndroidApp/SiPEKA/app/src/main/java/com/example/si_peka/MainActivity.java
@@ -1,54 +1,36 @@
 package com.example.si_peka;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import com.example.si_peka.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import org.jetbrains.annotations.NotNull;
-
-
 public class MainActivity extends AppCompatActivity {
-    BottomNavigationView bottomNavigationView;
-    private BottomNavigationView.OnNavigationItemSelectedListener navigation = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(MenuItem item) {
-            Fragment f = null;
-            switch (item.getItemId()){
-                case R.id.menu_home:
-                    f = new FragmentHome();
-                    break;
 
-                case R.id.menu_notif:
-                    f = new FragmentNotif();
-                    break;
-
-                case R.id.menu_cart:
-                    f = new FragmentCart();
-                    break;
-
-                case R.id.menu_profile:
-                    f = new FragmentProfile();
-                    break;
-            }
-            getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment,f).commit();
-            return true;
-        }
-    };
-
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        bottomNavigationView = findViewById(R.id.bottom_navigation_menu);
-        bottomNavigationView.setOnNavigationItemSelectedListener(navigation);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_cart, R.id.navigation_notifications, R.id.navigation_profile)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
 }
