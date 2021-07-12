25d16b8491749c36613732dc7f7ad33c5e9d9ac3
RhizkiPradhana <71806229+RhizkiPradhana@users.noreply.github.com>
2021-07-12 16:39:56 +0700

    bismillah

--- AndroidApp/SiPEKA/app/src/main/java/com/example/si_peka/MainActivity.java
+++ AndroidApp/SiPEKA/app/src/main/java/com/example/si_peka/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.si_peka;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -90,4 +99,58 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    GridView gridView;
+    String[] sayurNames = {"Sayur1","Sayur2","Sayur3","Sayur4","Sayur5","Sayur6"};
+    int[] sayurImages = {R.drawable.sayur1,R.drawable.sayur2,R.drawable.sayur3,R.drawable.sayur4,R.drawable.sayur5,R.drawable.sayur6,};
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gridView = findViewById(R.id.gridview);
+
+        CustomAdapter customAdapter =new CustomAdapter();
+        gridView.setAdapter(customAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),GridItemActivity.class);
+                intent.putExtra("name",sayurNames[1]);
+                intent.putExtra("image",sayurImages[1]);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private class CustomAdapter extends BaseAdapter  {
+        @Override
+        public int getCount() {
+            return sayurImages.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.baris_data,null);
+
+            TextView name = view.findViewById(R.id.sayur);
+            ImageView image = view.findViewById(R.id.image);
+
+            name.setText(sayurNames[1]);
+            image.setImageResource(sayurImages[1]);
+            return view;
+        }
+    }
 }
