25239fac3a70b65b427d745939ded6db24f8c3b8
RhizkiPradhana <71806229+RhizkiPradhana@users.noreply.github.com>
2021-07-13 11:30:26 +0700

    .

--- AndroidApp/SiPEKA/app/src/main/java/com/example/si_peka/ui/cart/CartFragment.java
+++ AndroidApp/SiPEKA/app/src/main/java/com/example/si_peka/ui/cart/CartFragment.java
@@ -1,9 +1,15 @@
 package com.example.si_peka.ui.cart;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,34 +18,67 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.si_peka.GridItemActivity;
+import com.example.si_peka.R;
 import com.example.si_peka.databinding.FragmentCartBinding;
 
 public class CartFragment extends Fragment {
 
-    private CartViewModel cartViewModel;
+    //    private CartViewModel cartViewModel;
     private FragmentCartBinding binding;
+    private GridItemActivity gridItemActivity;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        cartViewModel =
-                new ViewModelProvider(this).get(CartViewModel.class);
+    GridView gridView;
+    String[] sayurNames = {"Sayur1", "Sayur2", "Sayur3", "Sayur4", "Sayur5", "Sayur6"};
+    int[] sayurImages = {R.drawable.sayur1, R.drawable.sayur2, R.drawable.sayur3, R.drawable.sayur4, R.drawable.sayur5, R.drawable.sayur6,};
 
-        binding = FragmentCartBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        gridItemActivity.setContentView();
 
-        final TextView textView = binding.textCart;
-        cartViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        gridView = gridView.findViewById(R.id.gridview);
+
+        CustomAdapter customAdapter = new CustomAdapter();
+        gridView.setAdapter(customAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(gridItemActivity.getApplicationContext(), GridItemActivity.class);
+                intent.putExtra("name", sayurNames[1]);
+                intent.putExtra("image", sayurImages[1]);
+                startActivity(intent);
             }
         });
-        return root;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
+    private class CustomAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return sayurImages.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.baris_data, null);
+
+            TextView name = view.findViewById(R.id.sayur);
+            ImageView image = view.findViewById(R.id.image);
+
+            name.setText(sayurNames[1]);
+            image.setImageResource(sayurImages[1]);
+            return view;
+        }
 }
