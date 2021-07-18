8b0eb36329da6626753d7e4792b3f66d1db052dc
nidhakusuman <nidhakusumaningrum17@gmail.com>
2021-07-18 19:32:21 +0700

    z

---
+++ AndroidApp/SiPEKA/app/src/main/java/com/example/si_peka/DetailArtikel.java
@@ -0,0 +1,12 @@
+package com.example.si_peka;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailArtikel  extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_artikel);
+    }
+}
