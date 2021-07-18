8f274a5533c2baa8a6d8978adb972f2f3d903f57
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2021-07-18 09:14:34 +0700

    Update sadmin.blade.php

--- WebApi/resources/views/backend/layouts/sadmin.blade.php
+++ WebApi/resources/views/backend/layouts/sadmin.blade.php
@@ -103,7 +103,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="/sadmin" class="brand-link">
-      <img src="{{asset('assets/img/logo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{asset('assets/img/logo.png')}}" alt="AdminLTE Logo" class="brand-image" style="opacity: .8">
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
