55767bf0732bd6a29a97384a6043e1a5a9c32548
Moch. Fahrony <72066155+fahrony03@users.noreply.github.com>
2021-07-25 22:50:31 +0700

    android fix

---
+++ AndroidApp/SiPEKA/app/src/main/java/com/example/si_peka/DetailKategori1.java
@@ -0,0 +1,12 @@
+package com.example.si_peka;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailKategori1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kategori1);
+    }
+}
